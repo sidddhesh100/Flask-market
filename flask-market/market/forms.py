26fb16b5313b5193c1e1aeccc4b504c558cf0870
Siddhesh <siddheshangane142000@gmail.com>
2022-04-24 14:31:49 +0530

    Flask-market 1.0

---
+++ flask-market/market/forms.py
@@ -0,0 +1,40 @@
+from market.models import User
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+
+class RegisterForm(FlaskForm):
+
+    def validate_username(self, username_to_check):
+        user=User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError("username already exist please try different username")
+
+    def validate_email_address(self, email_address_to_check):
+            email=User.query.filter_by(email_address=email_address_to_check.data).first()
+            if email:
+                raise ValidationError("username already exist please try different username")
+
+
+    username=StringField(label="User Name:",validators=[Length(min=2,max=30),DataRequired()])
+    email_address=StringField(label="Email Address:",validators=[Email(),DataRequired()])
+    password1=PasswordField(label="Password:",validators=[Length(min=6),DataRequired()])
+    password2=PasswordField(label="Confirm Password:",validators=[EqualTo('password1'),DataRequired()])
+    submit=SubmitField(label="Create Account")
+
+class LoginForm(FlaskForm):
+    username=StringField(label="User Name:",validators=[DataRequired()])
+    password=PasswordField(label="Password:",validators=[DataRequired()])
+    submit=SubmitField(label="Sign in")
+
+
+class PurchaseItemForm(FlaskForm):
+    submit=SubmitField(label="Purchase item!")
+
+
+class SellItemForm(FlaskForm):
+    submit=SubmitField(label="Sell item!")
+
+
+
+
